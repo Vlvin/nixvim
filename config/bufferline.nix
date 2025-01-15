a0d661fe62cbf8999339e3d515ebf56dd2ea783b
vlvin <ghostv.work@gmail.com>
2025-01-15 12:17:27 +0400

    fixed secrets violation

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,6 +1,49 @@
-{
+# plugin configuration
+let 
+  secrets = import ./secrets.nix;
+in {
   plugins = {
     bufferline.enable = true;
     web-devicons.enable = true;
+    lightline.enable = true;
+    chadtree.enable = true;
+    floaterm.enable = true;
+    codecompanion = {
+      enable = true;
+      settings = {
+        adapters = {
+          anthropic = {
+            __raw = ''
+              function()
+                return require('codecompanion.adapters').extend('anthropic', {
+                    env = {
+                        api_key = "${secrets.api_token}",
+                   } 
+                })
+              end
+            '';
+          };
+        };
+        opts = {
+          log_level = "TRACE";
+          send_code = true;
+          use_default_actions = true;
+          use_default_prompts = true;
+        };
+        strategies = {
+          agent = {
+            adapter = "antropic";
+          };
+          chat = {
+            adapter = "antropic";
+          };
+          inline = {
+            adapter = "anthropic";
+          };
+        };
+      };
+
+    };
+    coq-nvim.enable = true;
   };
 }
--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,4 @@
-{self, ...}: {
+{self,  ...}: {
   # Import all your configuration modules here
   imports = [ ./bufferline.nix ];
   
@@ -28,63 +28,4 @@
       action = "<cmd>w<CR>";
     }
 ];
-  plugins = {
-    lightline.enable = true;
-    chadtree.enable = true;
-    treesitter = {
-      enable = true;
-      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-        nix
-        markdown
-        yaml
-      ];
-    };
-    codecompanion = {
-      enable = true;
-      settings = {
-        adapters = {
-          ollama = {
-            __raw = ''
-              function()
-                return require('codecompanion.adapters').extend('ollama', {
-                    env = {
-                        url = "http://127.0.0.1:11434",
-                    },
-                    schema = {
-                        model = {
-                            default = 'qwen2.5-coder:latest',
-                            -- default = "llama3.1:8b-instruct-q8_0",
-                        },
-                        num_ctx = {
-                            default = 32768,
-                        },
-                    },
-                })
-              end
-            '';
-          };
-        };
-        opts = {
-          log_level = "TRACE";
-          send_code = true;
-          use_default_actions = true;
-          use_default_prompts = true;
-        };
-        strategies = {
-          agent = {
-            adapter = "ollama";
-          };
-          chat = {
-            adapter = "ollama";
-          };
-          inline = {
-            adapter = "ollama";
-          };
-        };
-      };
-
-    };
-    coq-nvim.enable = true;
-  };
-
 }
