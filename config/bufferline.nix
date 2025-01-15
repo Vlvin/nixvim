1bc69769cf342ee76cad14d08914527e31fdcd74
vlvin <ghostv.work@gmail.com>
2025-01-15 12:54:25 +0400

    codeium cleanum

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,48 +1,16 @@
 # plugin configuration
-let 
-  secrets = import /etc/secrets/nixvim.nix;
-in {
+{
   plugins = {
     bufferline.enable = true;
     web-devicons.enable = true;
     lightline.enable = true;
     chadtree.enable = true;
     floaterm.enable = true;
-    codecompanion = {
+    codeium-nvim = {
       enable = true;
       settings = {
-        adapters = {
-          anthropic = {
-            __raw = ''
-              function()
-                return require('codecompanion.adapters').extend('anthropic', {
-                    env = {
-                        api_key = "${secrets.api_token}",
-                   } 
-                })
-              end
-            '';
-          };
-        };
-        opts = {
-          log_level = "TRACE";
-          send_code = true;
-          use_default_actions = true;
-          use_default_prompts = true;
-        };
-        strategies = {
-          agent = {
-            adapter = "antropic";
-          };
-          chat = {
-            adapter = "antropic";
-          };
-          inline = {
-            adapter = "anthropic";
-          };
-        };
+        enable_chat = true;
       };
-
     };
     coq-nvim.enable = true;
   };
--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,6 @@
-{self,  ...}: {
+{self, config, ...}: {
+  config.allowUnfree = true;
+
   # Import all your configuration modules here
   imports = [ ./bufferline.nix ];
   
