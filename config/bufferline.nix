2ccf9a594d77e882d34defc4da1c4fce02904ea8
vlvin <ghostv.work@gmail.com>
2025-01-15 12:57:00 +0400

    moved config to butterline.nix

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -14,4 +14,31 @@
     };
     coq-nvim.enable = true;
   };
+  colorschemes.gruvbox.enable = true;
+  opts = {
+    tabstop = 2;
+    shiftwidth = 2;
+    expandtab = true;
+    number = true;
+    relativenumber = true;
+    termguicolors = true;
+  
+    syntax = "on";
+  };
+
+  keymaps = [
+    {
+      key = "<C-t>";
+      action = "<cmd>CHADopen<CR>";
+    }
+    {
+      key = "<C-q>";
+      action = "<cmd>q<CR>";
+    }     
+    {
+      key = "<C-s>";
+      action = "<cmd>w<CR>";
+    }
+  ];
+
 }
--- config/default.nix
+++ config/default.nix
@@ -2,32 +2,5 @@
   config.allowUnfree = true;
 
   # Import all your configuration modules here
-  imports = [ ./bufferline.nix ];
-  
-  colorschemes.gruvbox.enable = true;
-  opts = {
-    tabstop = 2;
-    shiftwidth = 2;
-    expandtab = true;
-    number = true;
-    relativenumber = true;
-    termguicolors = true;
-  
-    syntax = "on";
-  };
-
-  keymaps = [
-    {
-      key = "<C-t>";
-      action = "<cmd>CHADopen<CR>";
-    }
-    {
-      key = "<C-q>";
-      action = "<cmd>q<CR>";
-    }     
-    {
-      key = "<C-s>";
-      action = "<cmd>w<CR>";
-    }
-];
+  imports = [ ./bufferline.nix ]; 
 }
